2feb5f6dc68c36ff090ceafb622245339eb7fce1
JuNicolle <juliennicolle@hotmail.fr>
2024-12-04 17:03:57 +0100

    auth

--- routes/crudTask.js
+++ routes/crudTask.js
@@ -1,63 +1,81 @@
-const express = require('express');
-const bdd = require('../bdd');
+const express = require("express");
+const bdd = require("../bdd");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
-
+const auth = require("../middleware/auth");
 
 // route creation tasks
-router.post('/createTask', (req, res) => {
-    const {nameTask, descriptionTask} = req.body;
-    const insertTask = "INSERT INTO tasks (nameTask, descriptionTask) VALUES (?,?);"
-    bdd.query(insertTask, [nameTask, descriptionTask], (error, results)=>{
-        if(error) throw error;
-        res.json(results);
-        // res.redirect('http://localhost:5173/createUser');
-    });
+router.post("/createTask", auth.authentification, (req, res) => {
+  console.log(req.userMail, req.userId);
+  const { nameTask, descriptionTask } = req.body;
+  const insertTask =
+    "INSERT INTO tasks (nameTask, descriptionTask, userTask) VALUES (?,?,?);";
+  bdd.query(
+    insertTask,
+    [nameTask, descriptionTask, req.userId],
+    (error, results) => {
+      if (error) throw error;
+      res.json(results);
+      // res.redirect('http://localhost:5173/createUser');
+    }
+  );
 });
 
-// route lecture des utilisateurs 
-router.get('/readTask', (req, res) => {
-    const readTasks = "SELECT * FROM tasks;"
-    bdd.query(readTasks, (error, results)=>{
-        if(error) throw error;
-        res.json(results);  
-    });
+// route lecture des utilisateurs
+router.get("/readTask", auth.authentification, (req, res) => {
+  const readTasks = "SELECT * FROM tasks WHERE userTask = ?;";
+  bdd.query(readTasks, [req.userId], (error, results) => {
+    if (error) throw error;
+    res.json(results);
+  });
 });
 
-// route mise à jour des utilisateurs
-router.post('/updateTask/:idTask', (req, res) => {
-    const {nameTask, descriptionTask} = req.body;
-    // les accolades ci dessus renvoient un fichier json 
-    const {idTask} = req.params;
-    const updateTask = "UPDATE tasks SET nameTask = ?, descriptionTask = ? WHERE idTask = ?;"
-    bdd.query(updateTask,[nameTask, descriptionTask, idTask], (error, results)=>{
-        if (error) throw error;
-        res.json(results);
-        // res.redirect('../update.html');
-    });
+// Route pour récupérer les tâches d'un utilisateur connecté
+router.get("/tasks", (req, res) => {
+  const sql = "SELECT * FROM tasks";
+  bdd.query(sql, (error, results) => {
+    if (error) throw error;
+    res.json(results);
+  });
+});
+
+// Route pour mettre à jour l'état d'une tâche
+router.get("/tasks", auth.authentification, (req, res) => {
+  const sql = "";
+  if (req.role === "admin") {
+    sql = "SELECT * FROM tasks";
+  } else {
+    sql = "SELECT * FROM tasks WHERE userTask = ?";
+  }
+
+  bdd.query(sql, [req.userId], (error, results) => {
+    if (error) throw error;
+    res.json(results);
+  });
 });
 
 // route validation d'une tâche
-router.post('/validateTask/:idTask', (req, res) => {
-    const { idTask } = req.params;
-    const newState = 2; 
+router.post("/validateTask/:idTask", (req, res) => {
+  const { idTask } = req.params;
+  const newState = 2;
 
-    const updateState = "UPDATE tasks SET idState = ? WHERE idTask = ?";
-    bdd.query(updateState, [newState, idTask], (error, results) => {
-        if (error) throw error;
-        res.json({ message: "Task updated successfully", results });
-    });
+  const updateState = "UPDATE tasks SET idState = ? WHERE idTask = ?";
+  bdd.query(updateState, [newState, idTask], (error, results) => {
+    if (error) throw error;
+    res.json({ message: "Task updated successfully", results });
+  });
 });
 
 // route suppression des utilisateurs
-router.delete('/deleteTask/:idTask', (req, res) => {
-    const { idTask } = req.params;
+router.delete("/deleteTask/:idTask", (req, res) => {
+  const { idTask } = req.params;
 
-    const deleteTask = "DELETE FROM tasks WHERE idTask = ?;";
-    bdd.query(deleteTask, [idTask], (error, results) => {
-        if (error) throw error;
-        res.json(results);
-        // res.redirect('/read.html');
-    });
+  const deleteTask = "DELETE FROM tasks WHERE idTask = ?;";
+  bdd.query(deleteTask, [idTask], (error, results) => {
+    if (error) throw error;
+    res.json(results);
+    // res.redirect('/read.html');
+  });
 });
 
 module.exports = router;
--- routes/crudUser.js
+++ routes/crudUser.js
@@ -1,96 +1,119 @@
-const express = require('express');
-const bdd = require('../bdd');
+const express = require("express");
+const bdd = require("../bdd");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
 
+// route creation utilisateur entre le code et la BDD
+router.post("/createUser", async (req, res) => {
+  const { nameUser, mailUser, passwordUser } = req.body;
 
-// route creation utilisateur entre le code et la BDD 
-router.post('/createUser', async (req, res) => {
-    const {nameUser, mailUser,passwordUser} = req.body;
-    // const securedPassword = await bcrypt.hash(passwordUser, 5);
-    const insertUser = "INSERT INTO users (nameUser, mailUser, passwordUser) VALUES (?,?,?);"
-    bdd.query(insertUser, [nameUser,mailUser,passwordUser], (error)=>{
-        if(error) throw error;
-        res.send('Utilisateur créé avec succès !');
-        // res.redirect('http://localhost:5173/createUser');
-    });
+  const hashedPassword = await bcrypt.hash(passwordUser, 5);
+
+  const insertUser =
+    "INSERT INTO users (nameUser, mailUser, passwordUser, role) VALUES (?,?,?,?);";
+  bdd.query(insertUser, [nameUser, mailUser, hashedPassword, "user"], (error) => {
+    if (error) throw error;
+    res.send("Utilisateur créé avec succès !");
+    // res.redirect('http://localhost:5173/createUser');
+  });
 });
 
 // route pour comparer le mot de passe entré par l'utilisateur avec celui enregistré dans la BDD
-router.post('/loginUser', (req, res) => {
-    const { mailUser, passwordUser } = req.body;
+router.post("/loginUser", (req, res) => {
+  const { mailUser, passwordUser } = req.body;
 
-    // Vérification des données envoyées
-    if (!mailUser || !passwordUser) {
-        return res.json({ error: "Email et mot de passe sont requis." });
-    }
+  // Vérification des données envoyées
+  if (!mailUser || !passwordUser) {
+    return res.json({ error: "Email et mot de passe sont requis." });
+  }
 
-    const checkUser = "SELECT mailUser, passwordUser FROM users WHERE mailUser = ?;";
+  const checkUser =
+    "SELECT idUser,mailUser, passwordUser FROM users WHERE mailUser = ?;";
 
-    bdd.query(checkUser, [mailUser], (error, results) => {
+  bdd.query(checkUser, [mailUser], (err, results) => {
+    if (err) throw err;
 
-        // Vérifier si l'utilisateur existe
-        if (results.length === 0) {
-            return res.send("Utilisateur non trouvé"); // RETURN pour arrêter l'exécution
-        }
-
-        // Récupérer les données de l'utilisateur
-        const user = results[0];
-
-        // Comparer les mots de passe
-        if (passwordUser === user.passwordUser) {
-            return res.send("Connexion réussie"); // RETURN pour arrêter l'exécution
+    if (results.length > 0) {
+      const user = results[0];
+      console.log(user);
+      bcrypt.compare(passwordUser, user.passwordUser, (err, result) => {
+        if (err) throw err;
+        if (result) {
+          const token = jwt.sign({ id: user.idUser, email: user.mailUser, role : user.role }, "secretkey", {
+            expiresIn: "1h",
+          });
+          res.json({ token });
         } else {
-            return res.send("Mot de passe incorrect"); // RETURN pour arrêter l'exécution
+          res.status(401).json({ error: "Email ou mot de passe incorrect" });
         }
-    });
+      });
+    } else {
+      res.status(404).send("Utilisateur non trouvé");
+    }
+  });
 });
 
+// Comparer les mots de passe
+//         if (passwordUser === user.passwordUser) {
+//             return res.send("Connexion réussie"); // RETURN pour arrêter l'exécution
+//         } else {
+//             return res.send("Mot de passe incorrect"); // RETURN pour arrêter l'exécution
+//         }
+//     });
+// });
 
-
-// route lecture des utilisateurs 
-router.get('/readUser', (req, res) => {
-    const readUser = "SELECT * FROM users;"
-    bdd.query(readUser, (error, results)=>{
-        if(error) throw error;
-        res.json(results);
-    });
+// route lecture des utilisateurs
+router.get("/readUser", (req, res) => {
+  const readUser = "SELECT * FROM users;";
+  bdd.query(readUser, (error, results) => {
+    if (error) throw error;
+    res.json(results);
+  });
 });
 
 //route lecteur d'un utilisateur par son ID
-router.get('/readUserById/:idUser', (req, res) => {
-    const {idUser} = req.params;
-    const readUser = "SELECT * FROM users WHERE idUser = ?;"
-    bdd.query(readUser, [idUser], (error, results)=>{
-        if(error) throw error;
-        res.json(results);
-    });
+router.get("/readUserById/:idUser", (req, res) => {
+  const { idUser } = req.params;
+  const readUser = "SELECT * FROM users WHERE idUser = ?;";
+  bdd.query(readUser, [idUser], (error, results) => {
+    if (error) throw error;
+    res.json(results);
+  });
 });
 
 // route mise à jour des utilisateurs
-router.post('/updateUser/:idUser', (req, res) => {
-    const {nameUser, mailUser,passwordUser} = req.body;
-    // les accolades ci dessus renvoient un fichier json 
-    const {idUser} = req.params;
-    const updateUser = "UPDATE users SET nameUser = ?, mailUser = ?, passwordUser = ? WHERE idUser = ?;"
-    bdd.query(updateUser,[nameUser, mailUser,passwordUser, idUser], (error, results)=>{
-        if (error) throw error;
-        res.json(results);
-        // res.redirect('../update.html');
-    });
+router.post("/updateUser/:idUser", (req, res) => {
+  const { nameUser, mailUser, passwordUser } = req.body;
+  // les accolades ci dessus renvoient un fichier json
+  const { idUser } = req.params;
+  const updateUser =
+    "UPDATE users SET nameUser = ?, mailUser = ?, passwordUser = ? WHERE idUser = ?;";
+  bdd.query(
+    updateUser,
+    [nameUser, mailUser, passwordUser, idUser],
+    (error, results) => {
+      if (error) throw error;
+      res.json(results);
+      // res.redirect('../update.html');
+    }
+  );
 });
 
 // route suppression des utilisateurs
-router.post('/deleteUser/:idUser', (req, res) => {
-    const { idUser } = req.params;
+router.post("/deleteUser/:idUser", (req, res) => {
+  if (req.role !== "admin") {
+    return res.status(401).send("Vous n'avez pas les droits pour supprimer un utilisateur");
+  }
+  const { idUser } = req.params;
 
-    const deleteUser = "DELETE FROM users WHERE idUser = ?;";
-    bdd.query(deleteUser, [idUser], (error, results) => {
-        if (error) throw error;
-        res.json(results);
-        // res.redirect('/read.html');
-    });
+  const deleteUser = "DELETE FROM users WHERE idUser = ?;";
+  bdd.query(deleteUser, [idUser], (error, results) => {
+    if (error) throw error;
+    res.json(results);
+    // res.redirect('/read.html');
+  });
 });
 
 module.exports = router;
